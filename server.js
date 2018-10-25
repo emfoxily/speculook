6466519128ac6a1ecbedcfe442d1ca6d7230e39c
emily reno <emilyreno@emilys-MacBook-Pro.local>
2018-10-25 18:57:20 -0400

    fixed user controller and added seed data

--- server.js
+++ server.js
@@ -15,13 +15,18 @@ const usersController = require('./controllers/users.js');
 app.use(express.json());
 app.use(express.static('public'));
 //Call Controller
-app.use('/speculook', usersController);
+app.use('/users', usersController);
 
 //Mongoose Middleware
 mongoose.connect('mongodb://localhost:27017/speculook', { useNewUrlParser: true });
 mongoose.connection.once('open', ()=>{
   console.log('Connected to Mongoose');
 });
+
+// Users.create(seed, (err, data) => {
+//   if (err) console.log(err.message)
+//   console.log('added users');
+// })
 
 //Listener
 app.listen(3000, ()=>{

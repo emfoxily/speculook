39c5e8ce49baabeac0fa1d7e1a32c716334a700d
emily reno <emilyreno@emilys-MacBook-Pro.local>
2018-10-25 17:37:18 -0400

    created user seed

---
+++ models/userSeed.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    username: 'emily',
+    password: 'luna',
+    admin: true
+  },
+  {
+    username: 'laura',
+    password: 'felix',
+    admin: true
+  },
+  {
+    username: 'alyssa',
+    password: 'ote',
+    admin: true
+  }
+]

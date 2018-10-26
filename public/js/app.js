4f98ceb4df0cdcabc3ec59be1b9ecf68502538c9
alyssackieffer <alyssackieffer@Alyssas-MacBook-Pro.local>
2018-10-26 19:16:33 -0400

    Changes to currentUser name and added Community Board feature

--- public/js/app.js
+++ public/js/app.js
@@ -82,7 +82,7 @@ app.controller('MainController', ['$http', function($http){
       url: '/speculook',
     }).then((response) => {
       console.log(response.data);
-      controller.loginSuccess = response.data.username;
+      controller.currentUser = response.data.username;
       controller.currentUserLocation = response.data.location;
       controller.currentUserInterests = response.data.interests;
       console.log(controller.loginSuccess);

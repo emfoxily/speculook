d7d77f65ea09ae06a866d22c364bb807f922f19c
emily reno <emilyreno@emilys-MacBook-Pro.local>
2018-10-25 20:48:17 -0400

    updated seed again

--- models/userSeed.js
+++ models/userSeed.js
@@ -3,18 +3,15 @@ module.exports = [
     username: 'emily',
     password: 'luna',
     admin: true
-    image:,
   },
   {
     username: 'laura',
     password: 'felix',
     admin: true,
-    image:,
   },
   {
     username: 'alyssa',
     password: 'ote',
     admin: true
-    image:,
   }
 ]

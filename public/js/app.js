b35c41725d0cbb2210a6e8a5d2ae333c0a541ba0
Laura Levenhagen <Laura@Lauras-MacBook-Pro.local>
2018-10-29 18:43:58 -0700

    fixed edit skills and mailbox

--- public/js/app.js
+++ public/js/app.js
@@ -173,41 +173,70 @@ this.showEditUserForm = (user, index) => {
 
 
 
-this.messageObject= {mail: ' ', sender: ' '};
-this.mailboxIds = [{id: ' ', mailboxMail: [ ]}];
+// this.messageObject= {mail: ' ', sender: ' '};
+// this.mailboxIds = [{id: ' ', mailboxMail: [ ]}];
+//
+// this.pushMessage = (message, userId) => {
+//   console.log(controller.users.length);
+//   let numOfUsers = controller.users.length;
+//   this.mailboxIds = [{id: ' ', mailboxMail: [' ' ]}];
+//   for (i=0; i<numOfUsers; i++) {
+//     console.log(i);
+//     this.mailboxIds[i] =
+//       { id: controller.users[i]._id,
+//         mailboxMail: []
+//       }
+//     ;
+//     // console.log(userId, this.mailboxIds[i].id);
+//     if (userId === this.mailboxIds[i].id) {
+//       console.log( this.mailboxIds[i].id + 'is the id of the mailbox for id ' + userId);
+//       this.mailboxIds[i].mailboxMail.unshift(  {
+//           mail: message,
+//           sender: 'need to add'
+//         });
+//       this.mailToSend = this.mailboxIds[i].mailboxMail;
+//       console.log(this.mailToSend);
+//       controller.editUser(userId, this.mailToSend);
+//     }
+//   }
+// }
 
-this.pushMessage = (message, userId) => {
-  console.log(controller.users.length);
-  let numOfUsers = controller.users.length;
-  this.mailboxIds = [{id: ' ', mailboxMail: [' ' ]}];
-  for (i=0; i<numOfUsers; i++) {
-    console.log(i);
-    this.mailboxIds[i] =
-      { id: controller.users[i]._id,
-        mailboxMail: []
-      }
-    ;
-    // console.log(userId, this.mailboxIds[i].id);
-    if (userId === this.mailboxIds[i].id) {
-      console.log( this.mailboxIds[i].id + 'is the id of the mailbox for id ' + userId);
-      this.mailboxIds[i].mailboxMail.unshift(  {
-          mail: message,
-          sender: 'need to add'
-        });
-      this.mailToSend = this.mailboxIds[i].mailboxMail;
-      console.log(this.mailToSend);
-      controller.editUser(userId, this.mailToSend);
+this.getMail = function(){
+  for (users in this.users){
+  $http({
+    method: 'GET',
+    url: '/users/user._id/messages' ,
+  }).then(response=>{
+    controller.users.mailbox = response.data
+    // console.log(controller.users)
+  });
+};
+}
+this.addMail = (user, message, sender)=> {
+  $http({
+    method: 'PUT',
+    url: '/users/' + user._id + '/messages',
+    data: {
+
+        mail: message,
+        sender: sender
+
     }
-  }
+  }).then((res)=> {
+    console.log(message);
+    this.getUsers();
+    // hide edit form
+    this.editIndex = null;
+  }, (err)=> {
+    console.log(err);
+  })
 }
 
-
-this.editUser = (id, mailboxMail) => {
+this.editUser = (id) => {
   $http({
     method: 'PUT',
     url: '/users/'+ id,
     data: {
-      mailbox: mailboxMail,
       name: this.editData.name,
       username: this.editData.username,
       password: this.editData.password,
@@ -219,21 +248,10 @@ this.editUser = (id, mailboxMail) => {
       github: this.editData.github,
       facebook: this.editData.facebook,
       interests: this.editData.interests,
-      iCanHelp: {
-              CSS: this.editData.iCanHelp.CSS,
-              HTML: this.editData.iCanHelp.HTML,
-              JavaScript: this.editData.iCanHelp.JavaScript,
-              jQuery: this.editData.iCanHelp.jQuery,
-              Angular: this.editData.iCanHelp.Angular,
-              CLI: this.editData.iCanHelp.CLI,
-              React: this.editData.iCanHelp.React,
-              Ruby: this.editData.iCanHelp.Ruby
-            }
+      iCanHelp: this.editData.iCanHelp
     }
   })
   .then((res)=> {
-    // console.log(mailboxMail);
-    // console.log(this.mail);
     this.getUsers();
     // hide edit form
     this.editIndex = null;
